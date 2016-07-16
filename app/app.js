6e64aa70bbd379b2285a1a4752b36e764910d577
Tim Wright <mithgirw@gmail.com>
2016-07-16 11:26:32 -0600

    menu bar sticks to top

--- app/app.js
+++ app/app.js
@@ -1,2 +1,18 @@
 "use strict";
 
+$(document).ready(function(){
+	var headerposition = $(".theheader").offset().top;
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > headerposition) {
+			$(".theheader").css({
+				position: 'fixed',
+				top: '0px'
+			});
+		}else {
+			$(".theheader").css({
+				position: 'static',
+				top: '0px'
+			});
+		}
+	});
+});
